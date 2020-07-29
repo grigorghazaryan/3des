3d2b7b5496e4ad82b0f599c4270ce82da60b544a
grigorghazaryan <grigorghazaryan@hotmail.com>
2020-07-29 18:37:36 +0400

    bug-fix-02

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -91,10 +91,3 @@ document.querySelector('.s-in').onclick = function() {
     document.querySelector('.sup').style.display = "none"
     document.querySelector('.sin').style.display = "block"
 }
-
-
-(function(w,d,v3){
-w.chaportConfig = {
-  appId : '5f2186fd90fc3c4af67bb893'
-};
-if(w.chaport)return;v3=w.chaport={};v3._q=[];v3._l={};v3.q=function(){v3._q.push(arguments)};v3.on=function(e,fn){if(!v3._l[e])v3._l[e]=[];v3._l[e].push(fn)};var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://app.chaport.com/javascripts/insert.js';var ss=d.getElementsByTagName('script')[0];ss.parentNode.insertBefore(s,ss)})(window, document);
